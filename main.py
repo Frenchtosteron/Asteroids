cf514021c975fa5673493755e6b50d9f2e301041
Frenchtosteron <ichani1@o2.pl>
2026-04-12 01:12:52 +0200

    2: White triangle in the middle

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     print("Starting Asteroids with pygame version: 3.13")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     while True:
@@ -12,6 +16,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        
+
+        
 if __name__ == "__main__":
     main()
